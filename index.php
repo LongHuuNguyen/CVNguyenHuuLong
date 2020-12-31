a3fb39121961d57603123d86791b89cf3a6a5b8c
NGUYEN HUU LONG <64796494+LongHuuNguyen@users.noreply.github.com>
2020-12-31 10:46:36 +0700

    fist commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Project CV</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div class="canvas-container">
+    <div class="column left">
+      <div id="title">Nguyen Huu Long
+
+        <div class="style_two">CV Resume</div>
+
+      </div>
+      <div class="heading">CONTACT
+        <a href="https://www.facebook.com/huulong.nguyen.7549/" target=" _blank"><i class="fab fa-facebook icon2"></i></a>
+        <a href="https://github.com/LongHuuNguyen" target=" _blank"><i class="fab fa-github icon2"></i></a>
+      </div>
+      <div class="sideList"><i class="fas fa-map-marker-alt icon"></i> DaNang,VietNam</div>
+      <div class="sideList"><i class="fas fa-mobile-alt icon"></i>0961707101</div>
+      <div class="sideList"><i class="far fa-envelope icon"></i>nhuulong3@gmail.com</div>
+      <div class="heading">REFERENCES</div>
+      <div class="text">Alex Trump</div>
+      <div class="subtext">trump123@gmail.com</div>
+
+      <div class="heading">PROGRAMMING LANGUAGES</div>
+      <div class="ratingTitle">C++
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">SQL
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">CSS
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">HTML
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">JavaScript
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">XML
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">Java
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">AngularJS
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">Swift
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">OWL
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">RDF/S
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+      </div>
+      <div class="ratingTitle">PHP
+        <div class="rating">
+          <i class="fas fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+          <i class="far fa-circle"></i>
+        </div>
+        <img src="https://scontent.fdad6-1.fna.fbcdn.net/v/t1.0-9/36448081_2090628004533325_3092085078243672064_o.jpg?_nc_cat=109&ccb=2&_nc_sid=a4a2d7&_nc_ohc=z6gDC-lpxuEAX-aN8LF&_nc_ht=scontent.fdad6-1.fna&oh=652e09fc3ed6c21ac44f2b68dfc6c41b&oe=5FEDD0C4" width="350" height="440" alt="">
+      </div>
+    </div>
+    <div class="column-right">
+      <div class="heading">TARGET</div>
+      <div class="text ">
+        <li>
+          I want to be an excellent employee.</li>
+        <li>A Professional working environment</li>
+        <li>Long-term commitment and dedication to the company</li>
+      </div>
+      <div class="heading">EDUCATION</div>
+      <div class="text education-section">09/2018 - 09/2022</div>
+      <i class="fas fa-book education-icon"></i>
+      <div class="education-title">Information Technology</div>
+      <div class="education-info">University of Science and Education Da Nang</div>
+      <div class="grade">Grade: 18CNTT4</div>
+      <div class="text">Modules:</div>
+      <div class="allModules">
+        <i class="far fa-dot-circle"></i>
+        <div class="module">
+          An object-oriented C ++ project:
+        </div>
+        <div class="module-info padding">
+          Building coffee shop staff management software
+        </div>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Functional programming
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Web Mapping and Analysis
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Javascript programming
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Object Oriented Programming with JaVa
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Web Programming
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Database and Information Systems
+        </div>
+        <br>
+        <i class="far fa-dot-circle"></i>
+        <div class="module padding">
+          Mobile programming
+        </div>
+        <br>
+      </div>
+      <div class="heading">STUDY PROCESS</div>
+      <div class="timeline-dates dateone">2018</div>
+      <div class="timeline-dates datetwo">2019</div>
+      <div class="timeline-dates datethree">2020</div>
+      <div class="timeline-dates datefour">2021</div>
+      <div class="timeline-dates datefive">2022</div>
+      <div class="timeline-one"></div>
+      <div class="date-one"></div>
+      <div class="date-two"></div>
+      <div class="date-three"></div>
+      <div class="date-four"></div>
+      <i class="fas fa-circle hb"></i>
+      <i class="fas fa-circle als"></i>
+      <i class="fas fa-circle aht"></i>
+      <i class="fas fa-circle sjl"></i>
+      <div class="jobs">
+        <div class="job blue">
+          <div class="job-title">
+            Basic and advanced C ++
+          </div>
+          <div class="job-description">
+            Build object-oriented and structured programming.
+          </div>
+        </div>
+        <div class="job lightgray">
+          <div class="job-title">
+            Basic and advanced JaVa
+          </div>
+          <div class="job-description">
+            Develop object-oriented programs and set up website presentation according to VMC model
+          </div>
+        </div>
+        <div class="job gray">
+          <div class="job-title">
+            Web Desig Wit HTML, CSS, JS & SQL
+          </div>
+          <div class="job-description">
+            Personal website design and administration
+          </div>
+        </div>
+        <div class="job red">
+          <div class="job-title">
+            Mobile programming
+          </div>
+          <div class="job-description">
+            Learn about Cross-Platform Mobile App Development Tools.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
